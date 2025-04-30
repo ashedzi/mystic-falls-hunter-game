5c989132eccce477d886e14dfad7bb2b0c511bcf
Ashedzi Solomon <solomonashe84@gmail.com>
2025-04-30 16:14:30 -0500

    Add final css style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,34 @@
-// 'use strict';
+'use strict';
 
-// // import * as utils from './utils.js';
+// import * as utils from './utils.js';
 
-// // const cards = utils.selectAll('.card');
-// // const back = utils.select('.back');
+// const cards = utils.selectAll('.card');
+// const back = utils.select('.back');
 
-// const cards = document.querySelectorAll('.card');
-// // const back = document.querySelector('.back');
+const cards = document.querySelectorAll('.card');
+// const back = document.querySelector('.back');
 
-// const images = [ 
-//     'elena.jpg', 
-//     'stefan.jpg', 
-//     'damon.jpg', 
-//     'bonnie.jpg', 
-//     'katherine.jpg', 
-//     'caroline.jpg', 
-//     'klaus.jpg', 
-//     'tyler.jpg'
-// ]; 
+const images = [ 
+    'elena.jpg', 
+    'stefan.jpg', 
+    'damon.jpg', 
+    'bonnie.jpg', 
+    'katherine.jpg', 
+    'caroline.jpg', 
+    'klaus.jpg', 
+    'tyler.jpg'
+]; 
 
-// const cardDeck = [...images, ...images];
-// const shuffledCards = cardDeck.sort(() => Math.random() - 0.5);
+const cardDeck = [...images, ...images];
+const shuffledCards = cardDeck.sort(() => Math.random() - 0.5);
+
+cards.forEach((card, index) => {
+    const back = card.querySelector('.back');
+    back.style.backgroundImage = `url('./assets/img/${shuffledCards[index]}')`;
+});
 
 // cards.forEach((card, index) => {
-//     const back = card.querySelector('.back');
-//     back.style.backgroundImage = `url('./assets/img/${shuffledCards[index]}')`;
+//     back.style.backgroundImage = `url('./img/cards/${shuffledCards[index]}')`;
+
+//     // card.classList.add('flipped');
 // });
-
-// // cards.forEach((card, index) => {
-// //     back.style.backgroundImage = `url('./img/cards/${shuffledCards[index]}')`;
-
-// //     // card.classList.add('flipped');
-// // });
